05b0e1d8995a78ca7148e5d7be4f0c2bd1faa0a6
tane-git <tane.alexander.wilson@gmail.com>
2022-11-20 20:15:57 +1300

    add dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,21 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
   imports: [
     BrowserModule, //
-    FormsModule, AppRoutingModule,
+    FormsModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
